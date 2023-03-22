152ecfa5b3c7f453c5eebf03b547a8f806ed7124
Erick Gustavo <erick.stazs@gmail.com>
2023-03-22 09:41:56 -0300

    feat: add default colors

---
+++ src/resources/theme/index.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  colors: {
+    background: '#F8F8F8',
+    text: '#29292E',
+
+    posts: {
+      background: '#FEFEFE',
+    },
+  },
+}
