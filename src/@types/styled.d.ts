dceb9fe864d9b846ac838bf57f76d765bd429b65
Erick Gustavo <erick.stazs@gmail.com>
2023-03-22 19:50:31 -0300

    chore: add new colors

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -4,7 +4,10 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
       background: string
+      primary: string
       text: string
+      icon: string
+
       posts: {
         background: string
       }
--- src/resources/theme/index.ts
+++ src/resources/theme/index.ts
@@ -4,6 +4,8 @@ export const theme: DefaultTheme = {
   colors: {
     background: '#F8F8F8',
     text: '#29292E',
+    primary: '#835AFD',
+    icon: '#737380',
 
     posts: {
       background: '#FEFEFE',
