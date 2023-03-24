9c61da8fdc7671fec423569623e97f4995b5a176
Erick Gustavo <erick.stazs@gmail.com>
2023-03-24 00:47:16 -0300

    feat: add UserIcon and User info component

--- src/pages/user-info/index.tsx
+++ src/pages/user-info/index.tsx
@@ -1,12 +1,90 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { ReactComponent as UserIcon } from '@/assets/icons/user.svg'
+import * as S from './styles'
+import { Title } from '@/components/title'
+
+type UserType = {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: {
+    street: string
+    suite: string
+    city: string
+    zipcode: string
+  }
+  phone: string
+  website: string
+}
 
 export function UserInfo () {
   const { id } = useParams<{ id: string }>()
+  const [user, setUser] = useState<UserType | null>(null)
+
+  useEffect(() => {
+    async function loadUser () {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        const data = await response.json()
+
+        setUser(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    loadUser()
+  }, [id])
 
   return (
     <div>
-      <h1>User Info</h1>
-      <p>User ID: {id}</p>
+      {user && (
+        <S.Container>
+          <header>
+            <h1>Olá, {user.name} 👋</h1>
+          </header>
+
+          <S.Section>
+            <div>
+              <h2>Informações</h2>
+
+              <S.ListContainer>
+                <li>Nome: <span>{user.name}</span></li>
+                <li>Usuário: <span>{user.username}</span></li>
+                <li>Email: <span>{user.email}</span></li>
+                <li>Telefone: <span>{user.phone}</span></li>
+                <li>Website: <span>{user.website}</span></li>
+              </S.ListContainer>
+            </div>
+
+            <div>
+              <h2>Endereço</h2>
+
+              <S.ListContainer>
+                <li>Rua: <span>{user.address.street}</span></li>
+                <li>Complemento: <span>{user.address.suite}</span></li>
+                <li>Cidade: <span>{user.address.city}</span></li>
+                <li>CEP: <span>{user.address.zipcode}</span></li>
+              </S.ListContainer>
+            </div>
+          </S.Section>
+        </S.Container>
+      )}
+
+      <S.Footer>
+        <Title>Recomendados</Title>
+
+        <S.UserList className='users-list'>
+          <S.Container>
+            <UserIcon aria-label="user" title="user" />
+            <strong>{user?.name}</strong>
+            <span>{user?.email}</span>
+            <span>{user?.website}</span>
+          </S.Container>
+        </S.UserList>
+      </S.Footer>
     </div>
   )
 }
---
+++ src/pages/user-info/styles.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 1.6rem;
+  margin: 0.8rem 0;
+  border-radius: 0.8rem;
+  background-color: #ffffff;
+  box-shadow: 0px 2px 12px rgba(0, 0, 0, 0.04);
+  color: ${({ theme }) => theme.colors.text};
+`
+
+export const Section = styled.section`
+  margin-bottom: 1.6rem;
+  display: flex;
+  gap: 2.4rem;
+  flex-wrap: wrap;
+
+  div {
+    flex: 1;
+    min-width: 260px;
+  }
+
+  h2 {
+    font-size: 2.4rem;
+    font-weight: 600;
+    margin-bottom: 1.6rem;
+  }
+`
+
+export const ListContainer = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  li {
+    color: ${({ theme }) => theme.colors.icon};
+  }
+
+  li + li {
+    margin-top: 1.6rem;
+  }
+
+  span {
+    color: ${({ theme }) => theme.colors.text};
+    margin-left: 0.8rem;
+  }
+`
+
+export const Footer = styled.footer`
+  margin-top: 2.4rem;
+`
+
+export const UserList = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+
+  div {
+    display: inline-flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    gap: 0.8rem;
+    padding: 1.6rem;
+    color: ${({ theme }) => theme.colors.icon};
+
+    strong {
+      display: block;
+      color: ${({ theme }) => theme.colors.text};
+    }
+  }
+`
