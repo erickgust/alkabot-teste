95454c6b902716b5a5bbb5819a9afa0ff8857d5c
Erick Gustavo <erick.stazs@gmail.com>
2023-03-24 10:27:32 -0300

    feat: redirect when user doesn't exists

--- src/pages/user-info/index.tsx
+++ src/pages/user-info/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useHistory, useParams } from 'react-router-dom'
 import { ReactComponent as UserIcon } from '@/assets/icons/user.svg'
 import * as S from './styles'
 import { Title } from '@/components/title'
@@ -20,6 +20,7 @@ type UserType = {
 }
 
 export function UserInfo () {
+  const history = useHistory()
   const { id } = useParams<{ id: string }>()
   const [user, setUser] = useState<UserType | null>(null)
   const [recommendedUsers, setRecommendedUsers] = useState<UserType[]>([])
@@ -28,16 +29,21 @@ export function UserInfo () {
     async function loadUser () {
       try {
         const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+
+        if (!response.ok) {
+          throw new Error('Usuário não encontrado')
+        }
+
         const data = await response.json()
 
         setUser(data)
       } catch (error) {
-        console.error(error)
+        history.push('/')
       }
     }
 
     loadUser()
-  }, [id])
+  }, [id, history])
 
   useEffect(() => {
     async function loadRecommendedUsers () {
